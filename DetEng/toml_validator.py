81c604587a58471fdddf3f4773b7a3ea2d54984d
Aquellis <36744677+Aquellis@users.noreply.github.com>
2025-06-12 23:29:52 +0300

    Debugging workflow

--- DetEng/toml_validator.py
+++ DetEng/toml_validator.py
@@ -15,6 +15,7 @@ for root, dirs, files in os.walk("custom_detections/"):
     # For every file found, check if it has the .toml extension
     # If it is a toml file, load its contents
     for file in files:
+        print(file)
         if file.endswith(".toml"):
     
             # Programmatically create the full file path for the TOML files
